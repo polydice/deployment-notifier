599cce453ff33280fa1c3c9c065e446c1c15215a
Stan Lo <a22301613@yahoo.com.tw>
2016-04-19 18:23:46 +0800

    Successfully send event to datadog

--- server.go
+++ server.go
@@ -9,9 +9,14 @@ import (
 
 	"github.com/google/go-github/github"
 	"github.com/julienschmidt/httprouter"
+	"github.com/zorkian/go-datadog-api"
 )
 
-type Event interface{}
+type GithubEvent struct {
+	Repo             *github.Repository
+	EventType        string
+	DeploymentStatus *github.DeploymentStatus
+}
 
 func main() {
 	route := httprouter.New()
@@ -30,10 +35,27 @@ func main() {
 
 func DeploymentHandler(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
 	event_type := req.Header.Get("X-Github-Event")
-	GetEvent(req, event_type)
+	event, err := GetEvent(req, event_type)
+
+	fmt.Printf("Received %v event of %v", event_type, *event.Repo.Name)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	datadog_client := GetDataDogClient()
+	datadog_event := &datadog.Event{
+		Title: "Received " + event_type + " event of " + *event.Repo.Name,
+		Text:  "Test",
+	}
+
+	_, datadog_err := datadog_client.PostEvent(datadog_event)
+	if err != nil {
+		fmt.Print(datadog_err)
+	}
 }
 
-func GetEvent(req *http.Request, event_type string) (Event, error) {
+func GetEvent(req *http.Request, event_type string) (GithubEvent, error) {
 	switch event_type {
 	case "deployment":
 		return decodeDeploymentEvent(req), nil
@@ -41,10 +63,18 @@ func GetEvent(req *http.Request, event_type string) (Event, error) {
 		return decodeDeploymentStatusEvent(req), nil
 	}
 
-	return nil, errors.New("Error: no matched event type")
+	return GithubEvent{}, errors.New("Error: no matched event type")
 }
 
-func decodeDeploymentStatusEvent(req *http.Request) github.DeploymentStatusEvent {
+func GetDataDogClient() *datadog.Client {
+	api_key := os.Getenv("DATADOG_API_KEY")
+	app_key := os.Getenv("DATADOG_APP_KEY")
+
+	client := datadog.NewClient(api_key, app_key)
+	return client
+}
+
+func decodeDeploymentStatusEvent(req *http.Request) GithubEvent {
 	decoder := json.NewDecoder(req.Body)
 
 	var event github.DeploymentStatusEvent
@@ -55,10 +85,16 @@ func decodeDeploymentStatusEvent(req *http.Request) github.DeploymentStatusEvent
 		fmt.Print(err)
 	}
 
-	return event
+	github_event := GithubEvent{
+		Repo:             event.Repo,
+		EventType:        "DeploymentStatus",
+		DeploymentStatus: event.DeploymentStatus,
+	}
+
+	return github_event
 }
 
-func decodeDeploymentEvent(req *http.Request) github.DeploymentEvent {
+func decodeDeploymentEvent(req *http.Request) GithubEvent {
 	decoder := json.NewDecoder(req.Body)
 
 	var event github.DeploymentEvent
@@ -69,5 +105,10 @@ func decodeDeploymentEvent(req *http.Request) github.DeploymentEvent {
 		fmt.Print(err)
 	}
 
-	return event
+	github_event := GithubEvent{
+		Repo:      event.Repo,
+		EventType: "DeploymentStatus",
+	}
+
+	return github_event
 }
