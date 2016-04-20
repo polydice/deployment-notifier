e088a3d13799d997169bbe2fdb2040cf4a8ef960
Stan Lo <a22301613@yahoo.com.tw>
2016-04-20 13:00:42 +0800

    Finish NewDatadogEvent()'s test

--- server_test.go
+++ server_test.go
@@ -1,12 +1,58 @@
 package main
 
 import (
+	"crypto/sha1"
 	"net/http"
 	"strings"
 	"testing"
 
+	"github.com/google/go-github/github"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestNewDatadogEventReturnsDeploymentEvent(t *testing.T) {
+	assert := assert.New(t)
+	test_repo_name := "polydice/test"
+	test_sha := string(sha1.New().Sum(nil))
+	test_event_name := test_repo_name + ":" + test_sha
+
+	event := GithubEvent{
+		Repo: &github.Repository{
+			FullName: &test_repo_name,
+		},
+		Deployment: &github.Deployment{
+			SHA: &test_sha,
+		},
+	}
+	datadog_event := NewDatadogEvent(&event)
+	assert.Contains(datadog_event.Title, test_event_name)
+}
+
+func TestNewDatadogEventReturnsDeploymentStatusEvent(t *testing.T) {
+	assert := assert.New(t)
+	test_repo_name := "polydice/test"
+	test_sha := string(sha1.New().Sum(nil))
+	test_event_name := test_repo_name + ":" + test_sha
+	test_target_url := "https://example.com"
+	test_state := "pending"
+
+	event := GithubEvent{
+		Repo: &github.Repository{
+			FullName: &test_repo_name,
+		},
+		Deployment: &github.Deployment{
+			SHA: &test_sha,
+		},
+		DeploymentStatus: &github.DeploymentStatus{
+			State:     &test_state,
+			TargetURL: &test_target_url,
+		},
+	}
+	datadog_event := NewDatadogEvent(&event)
+	assert.Contains(datadog_event.Title, test_event_name)
+	assert.Contains(datadog_event.Text, test_state)
+	assert.Contains(datadog_event.Text, test_target_url)
+}
 
 func TestGetEventReturnsDeploymentEvent(t *testing.T) {
 	assert := assert.New(t)
